3d4f333a74d8d777bf31d737ac8948614dea3f9d
nurgun_a <nurgun.alekseev@bk.ru>
2023-02-27 17:11:54 +0300

    Добавьте файлы проекта.

---
+++ Cs_Tovar_hw6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Cs_Tovar_hw6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "Домашнее задание № 6 (Товар)";
+            SetWindowSize(100, 40);
+
+            List<Prodact> l1=new List<Prodact>            
+            {
+                new Milk("Milk",14,34.6, DateTime.Now),
+                new Air_freshener("Air", 365,145.5,DateTime.Now),
+                new Bread("Bread", 5,35,DateTime.Now)
+            };
+            Prodact pr = new Milk("Milk2", 14, 56, DateTime.Now);
+            pr.Quantity = 50;
+
+            l1.Add(pr);
+
+            foreach (var item in l1)
+            {
+                item.Quantity = 100;
+                item.Print();
+                if (item is Milk) 
+                {
+                    (item as Milk).ShowMilk();
+                }
+                else if (item is Air_freshener)
+                {
+                    (item as Air_freshener).ShowAir();
+                }
+                WriteLine();
+            }         
+        }  
+    }
+}
---
+++ Cs_Tovar_hw6/Tovar.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Cs_Tovar_hw6
+{
+    class Tovar
+    {
+        string Name;
+        public Tovar() { }
+        public Tovar(string _Name) => Name = _Name;     
+        public void Print() => Write($"{Name}");
+    }
+    class Prodact: Tovar
+    {
+        double Price;
+        int Expiration;
+        DateTime dd;
+        public int Quantity { get; set; }
+        public Prodact(string _Name, int _Expiration, double _price, DateTime _dd)
+            : base(_Name)
+        {
+            Price = _price;
+            Expiration = _Expiration;
+            dd = _dd;
+        }
+        public new void Print() 
+        {
+            base.Print();
+            WriteLine($" - цена: {Price} руб., срок годности (дни): {Expiration}, дата изготовления: {dd.ToShortDateString()} кол.-во: {Quantity} ");
+        }
+    }
+    
+    class Milk : Prodact 
+    {
+        public Milk(string _Name, int _Expiration, double _price, DateTime _dd)
+            : base(_Name, _Expiration, _price,_dd) { }
+
+        public void ShowMilk() =>WriteLine($"Хранить при температуре +2, +6");
+    }
+    class Air_freshener:Prodact
+    {
+        public Air_freshener(string _Name, int _Expiration, double _price, DateTime _dd)
+            : base(_Name, _Expiration, _price,_dd) { }
+
+        public void ShowAir() => WriteLine($"Огнеопасно. Не хранить возле огня");
+    }
+    class Bread : Prodact
+    {
+        public Bread(string _Name, int _Expiration, double _price, DateTime _dd)
+            : base(_Name, _Expiration, _price, _dd) { }
+    }
+}
